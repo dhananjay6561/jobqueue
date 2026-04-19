4ad24a8997a1cad9011a608d7f6819baf7d55630
dhananjay6561 <dhananjayaggarwal6561@gmail.com>
2026-04-19 11:16:03 +0530

    add auth Zustand store

---
+++ frontend/src/store/authStore.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface AuthUser {
+  id: string
+  email: string
+  stripe_customer_id?: string
+}
+
+interface AuthState {
+  token: string | null
+  user: AuthUser | null
+  setAuth: (token: string, user: AuthUser) => void
+  clearAuth: () => void
+  isAuthenticated: () => boolean
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set, get) => ({
+      token: null,
+      user: null,
+      setAuth: (token, user) => set({ token, user }),
+      clearAuth: () => set({ token: null, user: null }),
+      isAuthenticated: () => !!get().token,
+    }),
+    {
+      name: 'jobqueue-auth',
+      partialize: (state) => ({ token: state.token, user: state.user }),
+    },
+  ),
+)
